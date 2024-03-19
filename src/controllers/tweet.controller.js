187957f0bfd9b6fce85e0b85f1382a2700d93239
Saurabh <saurabh.y@instantpay.in>
2024-03-19 22:57:40 +0530

    add tweet

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,13 +1,33 @@
+import { Tweet } from "../moduls/tweet.model.js";
+import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const addTweet = asyncHandler(async(req,res)=>{
     //TODO: create tweet
 
+    const {content} = req.body
+
+    if(!content){
+        throw new ApiError(400, "Content field is required");
+    }
+    const tweet = await Tweet.create(
+        {
+            content : content,
+            owner : req.user?._id
+        }
+    );
+
+    console.log(tweet);
+    const createdTweet = await Tweet.findById(tweet._id);
+     if(!createdTweet){
+        throw new ApiError(500 , "Something went adding Tweet")
+    }
+
     return res
     .status(200)
     .json(
-        new ApiResponse(200,{},"Tweet add successfully")
+        new ApiResponse(200,createdTweet,"Tweet add successfully")
     )
 })
 
