7dac3b684fa76a890905dbf114545970cefafe6f
Saurabh <saurabh.y@instantpay.in>
2024-03-20 22:45:35 +0530

    update tweet

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -31,18 +31,44 @@ const addTweet = asyncHandler(async(req,res)=>{
     )
 })
 
+const deleteTweet = asyncHandler(async(req,res)=>{
+
+})
+
 const updateTweet = asyncHandler(async(req,res)=>{
     //TODO: update tweet
+    const tweetId  = req.params.tweetId;
+    const {content}= req.body;
+console.log(tweetId);
+    if([content].some((field)=>field?.trim()==="")){
+        throw new ApiError(400 , "Content field is required")
+    }
+
+    if(!tweetId.replace(":","")){
+        throw new ApiError(400 , "Tweet id is required")
+    }
+
+    const tweet = await Tweet.findByIdAndUpdate(
+        tweetId.replace(":",""),
+        {
+            $set:{
+                content:content
+            }
+        },{
+            new:true
+        }
+    ) 
 
     return res
     .status(200)
     .json(
-        new ApiResponse(200,{},"Tweet Update successfully")
+        new ApiResponse(200,tweet,"Tweet Update successfully")
     )
 })
 
 
 export{
     addTweet,
-    updateTweet
+    updateTweet,
+    deleteTweet
 }
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { addTweet } from "../controllers/tweet.controller.js";
+import { addTweet, deleteTweet, updateTweet } from "../controllers/tweet.controller.js";
 
 const router = Router();
 
 
 router.route("/").post(addTweet)
+router.route("/:tweetId").patch(updateTweet)
+router.route("/:tweetId").delete(deleteTweet)
 
 export default router
